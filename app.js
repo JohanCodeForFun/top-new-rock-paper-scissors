ca55f484b0ac34aee8a91adcb612c06b7cadf414
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-01 19:56:32 +0200

    create hands and function to randomly generate number ranging from 0-2

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const hands = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3);
+    console.log(computerChoice);
+}
+
+getComputerChoice();
