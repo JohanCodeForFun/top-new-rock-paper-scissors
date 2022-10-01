a76c4fa8a22ba3601932626e4f93bc12ae8c73a8
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-01 20:03:37 +0200

    create function to randomly generate computer hand choice from hands array

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const hands = ['rock', 'paper', 'scissors'];
 
 function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3);
-    console.log(computerChoice);
+    const randomComputerChoice = Math.random();
+    const totalHands = hands.length;
+    const randomIndex = Math.floor(randomComputerChoice * totalHands);
+    const randomComputerHand = hands[randomIndex];
+    console.log(randomComputerHand);
+    
+    // const computerChoice = Math.floor(Math.random() * 3);
+    // console.log(computerChoice);
 }
 
 getComputerChoice();
