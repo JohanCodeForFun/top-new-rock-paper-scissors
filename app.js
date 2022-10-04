4123809ebbaa435689b47096363067948171b83f
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-04 21:44:58 +0200

    DONE with main game!

--- app.js
+++ app.js
@@ -7,62 +7,64 @@ function game() {
 
   const playRound = function () {
     for (let i = 1; i <= 5; i++) {
-      console.log("--------------")
-      console.log("Round: "  +i);
-      console.log(`Player score: ${playerScore}`)
-      console.log(`Computer score: ${computerScore}`)
-      console.log("--------------")
+      console.log("--------------");
+      console.log("Round: " + i);
+      console.log(`Player score: ${playerScore}`);
+      console.log(`Computer score: ${computerScore}`);
+      console.log("--------------");
 
+      const getComputerChoice = function () {
+        const randomComputerChoice = Math.random();
+        const totalHands = hands.length;
+        const randomIndex = Math.floor(randomComputerChoice * totalHands);
+        const computerChoice = hands[randomIndex];
 
-    const getComputerChoice = function () {
-      const randomComputerChoice = Math.random();
-      const totalHands = hands.length;
-      const randomIndex = Math.floor(randomComputerChoice * totalHands);
-      const computerChoice = hands[randomIndex];
+        console.log(`Computer choose: ${computerChoice}`);
+        return computerChoice;
+      };
 
-      console.log(`Computer choose: ${computerChoice}`);
-      return computerChoice;
-    };
+      const getPlayerChoice = function () {
+        let playerChoice = prompt("Choose rock, paper or scissors");
 
-    const getPlayerChoice = function () {
-      let playerChoice = prompt("Choose rock, paper or scissors");
+        console.log(`Player choose: ${playerChoice}`);
+        return playerChoice;
+      };
 
-      console.log(`Player choose: ${playerChoice}`);
-      return playerChoice;
-    };
-
-    function decideWinningHand(computerChoice, playerChoice) {
-      console.log(
-        `computer choose: ${computerChoice} while player choose: ${playerChoice}`
-      );
-      if (computerChoice === playerChoice) {
-        console.log("It's a tie!");
-        return;
-      } else {
-        if (computerChoice === "rock" && playerChoice === "paper") {
-          console.log("Player wins! Paper beats rock");
-          playerScore++;
-          return;
-        } else if (computerChoice === "paper" && playerChoice === "scissors") {
-          console.log("Player wins! Scissors beats paper");
-          playerScore++;
-          return;
-        } else if (computerChoice === "scissors" && playerChoice === "rock") {
-          console.log("Player wins! Rock beats scissors");
-          playerScore++;
+      function decideWinningHand(computerChoice, playerChoice) {
+        console.log(
+          `computer choose: ${computerChoice} while player choose: ${playerChoice}`
+        );
+        if (computerChoice === playerChoice) {
+          console.log("It's a tie!");
           return;
         } else {
-          console.log("Computer wins!");
-          computerScore++;
-          return;
+          if (computerChoice === "rock" && playerChoice === "paper") {
+            console.log("Player wins! Paper beats rock");
+            playerScore++;
+            return;
+          } else if (
+            computerChoice === "paper" &&
+            playerChoice === "scissors"
+          ) {
+            console.log("Player wins! Scissors beats paper");
+            playerScore++;
+            return;
+          } else if (computerChoice === "scissors" && playerChoice === "rock") {
+            console.log("Player wins! Rock beats scissors");
+            playerScore++;
+            return;
+          } else {
+            console.log("Computer wins!");
+            computerScore++;
+            return;
+          }
         }
       }
+
+      decideWinningHand(getComputerChoice(), getPlayerChoice());
     }
-
-    decideWinningHand(getComputerChoice(), getPlayerChoice());
   };
-}
-  playRound();
 
+  playRound();
 }
 game();
