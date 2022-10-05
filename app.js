8765c680ba9069a8bcb29bc3d2bb96acab4b58a8
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-10-05 12:59:21 +0200

    add button and function to click it and get a function

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-window.onload = (event) => {
-  console.log('Get Ready! The Game will start in...');
-  console.log('3... 2... 1...');
-  
-  setTimeout(() => {
-    game();
-  }, 3000)
-};
+// window.onload = (event) => {
+//   console.log("Get Ready! The Game will start in...");
+//   console.log("3... 2... 1...");
+
+//   setTimeout(() => {
+//     game();
+//   }, 3000);
+// };
+
+const btnRock = document.getElementById('rock');
+const btnPaper = document.getElementById('paper');
+const btnScissors = document.getElementById('scissors');
+
+btnRock.addEventListener('click', function() {
+  console.log('click rock btn');
+})
+btnPaper.addEventListener('click', function() {
+  console.log('click paper btn');
+})
+btnScissors.addEventListener('click', function() {
+  console.log('click scissors btn');
+})
 
 
 function game() {
@@ -16,64 +30,52 @@ function game() {
   let playerScore = 0;
 
   const playRound = function () {
-    for (let i = 1; i <= 5; i++) {
-      console.log("--------------");
-      console.log("Round: " + i);
-      console.log(`Player score: ${playerScore}`);
-      console.log(`Computer score: ${computerScore}`);
-      console.log("--------------");
 
-      const getPlayerChoice = function () {
-        let playerChoice = prompt("Choose rock, paper or scissors");
-
-        // remove after validation function is done
-        // console.log(`Player choose: ${playerChoice}`);
-        return playerChoice;
-      };
-
-      const getComputerChoice = function () {
-        const randomComputerChoice = Math.random();
-        const totalHands = hands.length;
-        const randomIndex = Math.floor(randomComputerChoice * totalHands);
-        const computerChoice = hands[randomIndex];
-        return computerChoice;
-      };
-
-      function decideWinningHand(computerChoice, playerChoice) {
-        console.log(`Computer choose: ${computerChoice}`);
-        console.log(`Player choose: ${playerChoice}`);
-
-        if (computerChoice === playerChoice) {
-          console.log("It's a tie!");
-          return;
-        } else {
-          if (computerChoice === "rock" && playerChoice === "paper") {
-            console.log("Player wins! Paper beats rock");
-            playerScore++;
-            return;
-          } else if (
-            computerChoice === "paper" &&
-            playerChoice === "scissors"
-          ) {
-            console.log("Player wins! Scissors beats paper");
-            playerScore++;
-            return;
-          } else if (computerChoice === "scissors" && playerChoice === "rock") {
-            console.log("Player wins! Rock beats scissors");
-            playerScore++;
-            return;
-          } else {
-            console.log(
-              `Computer wins! ${computerChoice} beats ${playerChoice}`
-            );
-            computerScore++;
-            return;
-          }
-        }
-      }
-      decideWinningHand(getComputerChoice(), getPlayerChoice());
-    }
   };
-  playRound();
-}
 
+  const getPlayerChoice = function () {
+    let playerChoice = prompt("Choose rock, paper or scissors");
+
+    // remove after validation function is done
+    // console.log(`Player choose: ${playerChoice}`);
+    return playerChoice;
+  };
+
+  const getComputerChoice = function () {
+    const randomComputerChoice = Math.random();
+    const totalHands = hands.length;
+    const randomIndex = Math.floor(randomComputerChoice * totalHands);
+    const computerChoice = hands[randomIndex];
+    return computerChoice;
+  };
+
+  function decideWinningHand(computerChoice, playerChoice) {
+    console.log(`Computer choose: ${computerChoice}`);
+    console.log(`Player choose: ${playerChoice}`);
+
+    if (computerChoice === playerChoice) {
+      console.log("It's a tie!");
+      return;
+    } else {
+      if (computerChoice === "rock" && playerChoice === "paper") {
+        console.log("Player wins! Paper beats rock");
+        playerScore++;
+        return;
+      } else if (computerChoice === "paper" && playerChoice === "scissors") {
+        console.log("Player wins! Scissors beats paper");
+        playerScore++;
+        return;
+      } else if (computerChoice === "scissors" && playerChoice === "rock") {
+        console.log("Player wins! Rock beats scissors");
+        playerScore++;
+        return;
+      } else {
+        console.log(`Computer wins! ${computerChoice} beats ${playerChoice}`);
+        computerScore++;
+        return;
+      }
+    }
+  }
+  decideWinningHand(getComputerChoice(), getPlayerChoice());
+}
+playRound();
